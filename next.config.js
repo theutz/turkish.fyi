1b0a6861256b64a2a44fea5466bd4332565117b8
Michael Utz <michael@theutz.com>
2021-07-25 16:34:05 +0300

    Setup MDX
    Add types for @mdx-js/react

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
-module.exports = {
+const withMDX = require("@next/mdx");
+
+module.exports = withMDX({ extension: /\.mdx?$/ })({
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   reactStrictMode: true,
-}
+});
