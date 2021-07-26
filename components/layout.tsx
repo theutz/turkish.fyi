c0c510f45434aa18a8462b7b5cb617400b405450
Michael Utz <michael@theutz.com>
2021-07-27 01:43:48 +0300

    Work on /postpositions layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import Link from "../components/Link";
 import { useSiteTitle, useSiteDescription } from "../hooks/useSiteData";
+import { MDXProvider } from "@mdx-js/react";
+import { components } from "../components/theme";
 import Head from "../components/Head";
 import Header from "../components/Header";
 
@@ -8,9 +9,12 @@ type Props = { children: React.ReactNode };
 
 export default function Layout({ children }: Props) {
   return (
-    <div>
-
-      <main className="container px-4 mx-auto">{children}</main>
-    </div>
+    <MDXProvider components={{ ...components }}>
+      <>
+        <Head />
+        <Header />
+        <main className="container px-4 mx-auto">{children}</main>
+      </>
+    </MDXProvider>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import { SiteDataProvider } from "../hooks/useSiteData";
-import { MDXProvider } from "@mdx-js/react";
-import { components } from "../components/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SiteDataProvider>
-      <MDXProvider components={components}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </MDXProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SiteDataProvider>
   );
 }
