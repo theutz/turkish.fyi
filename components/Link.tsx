e8dee28ccf3d7d3075d4a81ada4292b323a296f0
Michael Utz <michael@theutz.com>
2021-07-26 15:30:25 +0300

    Make a link component

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import NextLink, { LinkProps } from "next/link";
+import classnames from "classnames";
 
 export type Props = LinkProps & {
   children: ReactNode;
@@ -8,7 +9,7 @@ export type Props = LinkProps & {
 export function Link({ children, className, ...props }: Props) {
   return (
     <NextLink {...props}>
-      <a className={className}>{children}</a>
+      <a className={classnames(className)}>{children}</a>
     </NextLink>
   );
 }
