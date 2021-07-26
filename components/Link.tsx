a02ed011ac81fabad4dcaab3e2e63aa47b4fa752
Michael Utz <michael@theutz.com>
2021-07-26 16:09:10 +0300

    Fix classname being purged by tailwind in prod

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,15 +1,20 @@
 import { ReactNode } from "react";
 import NextLink, { LinkProps } from "next/link";
+import classNames from "classnames";
 
 export type Props = LinkProps & {
   children: ReactNode;
-  color: `white` | `red-600`;
+  colorClassName: `text-white` | `text-red-600`;
 };
 
-export function Link({ children, color = "red-600", ...props }: Props) {
+export function Link({
+  children,
+  colorClassName = "text-red-600",
+  ...props
+}: Props) {
   return (
     <NextLink {...props}>
-      <a className={`text-${color}`}>{children}</a>
+      <a className={classNames(colorClassName)}>{children}</a>
     </NextLink>
   );
 }
