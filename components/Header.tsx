0784e7ae06dd8ea35ba6834211e356183e97440c
Michael Utz <michael@theutz.com>
2021-07-27 01:42:26 +0300

    Extract <Header /> from <Layout />

---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import { useSiteTitle, useSiteDescription } from "../hooks/useSiteData";
+import Link from "../components/Link";
+
+type Props = {
+  children: ReactNode;
+};
+
+export function Header({ children }: Props) {
+  const [title] = useSiteTitle();
+  return (
+    <header className="mb-4 text-white bg-gradient-to-b from-red-600 to-red-500">
+      <div className="container px-4 py-2 mx-auto">
+        <h1 className="text-xl">
+          <Link href="/" colorClassName="text-white">
+            {title}
+          </Link>
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- components/layout.tsx
+++ components/layout.tsx
@@ -2,23 +2,13 @@ import React from "react";
 import Link from "../components/Link";
 import { useSiteTitle, useSiteDescription } from "../hooks/useSiteData";
 import Head from "../components/Head";
+import Header from "../components/Header";
 
 type Props = { children: React.ReactNode };
 
 export default function Layout({ children }: Props) {
-  const [title] = useSiteTitle();
-
   return (
     <div>
-      <header className="bg-gradient-to-b from-red-600 to-red-500 text-white mb-4">
-        <div className="container px-4 py-2 mx-auto">
-          <h1 className="text-xl">
-            <Link href="/" colorClassName="text-white">
-              {title}
-            </Link>
-          </h1>
-        </div>
-      </header>
 
       <main className="container px-4 mx-auto">{children}</main>
     </div>
