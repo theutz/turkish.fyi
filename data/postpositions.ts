d6768908b6148a4be0aa01c016d29fef816efd45
Michael Utz <michael@theutz.com>
2021-07-31 02:23:42 +0300

    Finished spatial postpositions

--- data/postpositions.ts
+++ data/postpositions.ts
@@ -78,12 +78,103 @@ const bareDative: Postpositions = [
       ["Ankara'ya kadar", ["as far as Ankara"]],
     ],
   },
+  {
+    word: ["karşi", ["facing", "towards (of attitudes/feelings)", "against"]],
+    kind: "bare",
+    group: "dative",
+    examples: [
+      ["denize karşi", ["facing the sea"]],
+      ["bana karşi", ["towards me"]],
+      ["sigaraya karşi", ["against smoking"]],
+    ],
+  },
+  {
+    word: ["rağmen/karşin", ["in spite of"]],
+    kind: "bare",
+    group: "dative",
+    examples: [["soğuğa rağmen", ["in spite of the cold"]]],
+  },
+];
+
+const bareAblative: Postpositions = [
+  {
+    word: ["beri", ["since", "for (amount of time)"]],
+    kind: "bare",
+    group: "ablative",
+    examples: [
+      ["dün akşamdan beri", ["since yesterday evening"]],
+      ["birkaç günden beri", ["for several days (now)"]],
+    ],
+  },
+  {
+    word: ["bu yana", ["since"]],
+    kind: "bare",
+    group: "ablative",
+    examples: [["mayıstan bu yana", ["since May"]]],
+  },
+  {
+    word: ["önce", ["before"]],
+    kind: "bare",
+    group: "ablative",
+    examples: [["ondan önce", ["before that"]]],
+  },
+  {
+    word: ["sonra", ["after"]],
+    kind: "bare",
+    group: "ablative",
+    examples: [["dersten sonra", ["after the lesson"]]],
+  },
+  {
+    word: ["başka", ["apart from"]],
+    kind: "bare",
+    group: "ablative",
+    examples: [["senden başka", ["apart from you"]]],
+  },
+  {
+    word: ["dolayı", ["because of"]],
+    kind: "bare",
+    group: "ablative",
+    examples: [["uzmanlığından dolayı", ["because of his/her expertise"]]],
+  },
 ];
 
 const posessiveSpatial: Postpositions = [
   {
-    word: ["üstünde", ["on", "on top of", "above"]],
+    root: ["ön", ["front"]],
+    word: ["önünde", ["in front of"]],
+    kind: "posessive",
+    group: "spatial",
+    examples: [["", [""]]],
+  },
+  {
+    root: ["arka/ard", ["back"]],
+    word: ["arkasında/ardında", ["behind"]],
+    kind: "posessive",
+    group: "spatial",
+    examples: [["", [""]]],
+  },
+  {
+    root: ["iç", ["interior"]],
+    word: ["içinde", ["in", "inside"]],
+    kind: "posessive",
+    group: "spatial",
+    examples: [
+      [
+        "Bahçeye ulaşmak için evin içinden geçmek gerekiyordu",
+        ["In order to reach the garden, one has to go through the house."],
+      ],
+    ],
+  },
+  {
+    root: ["diş", ["exterior"]],
+    word: ["dışında", ["outside"]],
+    kind: "posessive",
+    group: "spatial",
+    examples: [["", [""]]],
+  },
+  {
     root: ["üst", ["top"]],
+    word: ["üstünde", ["on", "on top of", "above"]],
     kind: "posessive",
     group: "spatial",
     examples: [
@@ -94,8 +185,29 @@ const posessiveSpatial: Postpositions = [
     ],
   },
   {
-    word: ["arasında", ["between", "among"]],
+    root: ["alt", ["bottom"]],
+    word: ["altında", ["under", "underneath"]],
+    kind: "posessive",
+    group: "spatial",
+    examples: [["", [""]]],
+  },
+  {
+    root: ["yan", ["side"]],
+    word: ["yanında", ["beside", "next to"]],
+    kind: "posessive",
+    group: "spatial",
+    examples: [["", [""]]],
+  },
+  {
+    root: ["karşı", ["opposite side"]],
+    word: ["karşısında", ["opposite"]],
+    kind: "posessive",
+    group: "spatial",
+    examples: [["", [""]]],
+  },
+  {
     root: ["ara", ["space", "interval"]],
+    word: ["arasında", ["between", "among"]],
     kind: "posessive",
     group: "spatial",
     examples: [
@@ -105,10 +217,25 @@ const posessiveSpatial: Postpositions = [
       ],
     ],
   },
+  {
+    root: ["etraf/çevre", ["surroundings"]],
+    word: ["etrafında/çevresinde", ["around"]],
+    kind: "posessive",
+    group: "spatial",
+    examples: [["", [""]]],
+  },
+  {
+    root: ["öte", ["far side"]],
+    word: ["ötesinde", ["beyond"]],
+    kind: "posessive",
+    group: "spatial",
+    examples: [["", [""]]],
+  },
 ];
 
 export const postpositions: Postpositions = [
   ...bareNoneOrGenitive,
   ...bareDative,
+  ...bareAblative,
   ...posessiveSpatial,
 ];
